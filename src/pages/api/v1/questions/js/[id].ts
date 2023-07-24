dc514a4ec2f2ebf260d2fc8ca7a650c7fa9df33b
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-24 15:17:54 -0300

    Añadido: PUT funcionando

--- src/pages/api/v1/questions/js/[id].ts
+++ src/pages/api/v1/questions/js/[id].ts
@@ -2,31 +2,63 @@ import clientPromise from "@/libs/connectdb";
 import { ObjectId } from "mongodb";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const id: any = req.query.id;
 
   if (req.method === "GET") {
     try {
       const client = await clientPromise;
       const db = client.db("interview_questions");
-  
-      const js_question = await db.collection("javascript").find({ _id: new ObjectId(id) }).toArray();
+
+      const js_question = await db
+        .collection("javascript")
+        .find({ _id: new ObjectId(id) })
+        .toArray();
 
       if (js_question.length === 0) {
-        throw new Error("Doesn't exist this ID")
+        throw new Error("Doesn't exist this ID");
       }
 
-      const js = js_question[0]
-  
+      const js = js_question[0];
+
       return res.status(200).json({ js });
     } catch (error: any) {
-      return res.status(202).json({ error: error.message })
+      return res.status(202).json({ error: error.message });
     }
   }
   if (req.method === "PUT") {
-    
+    try {
+      const client = await clientPromise;
+      const db = client.db("interview_questions");
+
+      const put_question = {
+        question: req.body.question,
+        answer: req.body.answer,
+      };
+
+      const updated_question = await db
+        .collection("javascript")
+        .updateOne(
+          { _id: new ObjectId(id) },
+          { $set: put_question }
+        );
+
+      return res
+        .status(200)
+        .json({
+          status: "successful",
+          _id: id,
+          question: put_question.question,
+          answer: put_question.answer,
+        });
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
   }
   if (req.method === "DELETE") {
-    
+
   }
 }
--- src/pages/api/v1/questions/ts/[id].ts
+++ src/pages/api/v1/questions/ts/[id].ts
@@ -24,7 +24,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   }
   if (req.method === "PUT") {
-    
+    try {
+      const client = await clientPromise;
+      const db = client.db("interview_questions");
+
+      const put_question = {
+        question: req.body.question,
+        answer: req.body.answer,
+      };
+
+      const updated_question = await db
+        .collection("typescript")
+        .updateOne(
+          { _id: new ObjectId(id) },
+          { $set: put_question }
+        );
+
+      return res
+        .status(200)
+        .json({
+          status: "successful",
+          _id: id,
+          question: put_question.question,
+          answer: put_question.answer,
+        });
+    } catch (error: any) {
+      return res.status(400).json({ error: error.message });
+    }
   }
   if (req.method === "DELETE") {
     
