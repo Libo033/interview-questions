89b0b7345c9b22a3013c938efd0a6032bafed8fc
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-26 12:19:20 -0300

    Creado: UpdateForm - Componente Responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import CreateForm from '@/components/CreateForm'
 import React from 'react'
+import UpdateForm from '@/components/UpdateForm'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={inter.className + " pagina"}>
-      <CreateForm theme={'ligth'} lang={'JavaScript'} />
+      <UpdateForm theme={'ligth'} lang={'JavaScript'} />
     </div>
   )
 }
