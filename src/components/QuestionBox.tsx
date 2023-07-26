f6bbb5dcca5f2f808a68c94b74f3083d3cf91a3b
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-26 14:13:46 -0300

    Añadido: QuestionBox - Componente Responsive

--- src/components/QuestionBox.tsx
+++ src/components/QuestionBox.tsx
@@ -1,9 +1,13 @@
 import { IQuestionBox } from '@/libs/interfaces'
 import React from 'react'
+import styles from '@/styles/Componentes.module.css'
 
-const QuestionBox: React.FC<IQuestionBox> = () => {
+const QuestionBox: React.FC<IQuestionBox> = ( props ) => {
   return (
-    <div>QuestionBox</div>
+    <div className={styles.questionBox}>
+      <p className={styles.question}>{props.num} - {props.question}</p>
+      <p className={styles.answer}>{props.answer}</p>
+    </div>
   )
 }
 
