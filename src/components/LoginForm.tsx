4cdd873c850973e92de58a453dae1630ca6049b8
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-07-26 14:44:42 -0300

    Añadido: LoginForm - Componente Responsive

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,37 @@
 import { TypeTheme } from '@/libs/interfaces'
 import React from 'react'
+import styles from '@/styles/Componentes.module.css'
 
 const LoginForm: React.FC<TypeTheme> = ( props ) => {
   return (
-    <div>LoginForm</div>
+    <div className={styles.createForm}>
+      <p className={styles.formTitle}>Ingresar al Dashboard</p>
+      <form className={styles.form} method="post">
+        <div className={styles.formLabelBoxLogin}>
+          <label className={styles.formLabel} htmlFor="email">
+            EMAIL
+          </label>
+          <input
+            className={styles.fromInput}
+            id="email"
+            type="text"
+            autoComplete="off"
+          />
+        </div>
+        <div className={styles.formLabelBoxLogin}>
+          <label className={styles.formLabel} htmlFor="password">
+            CONTRASEÑA
+          </label>
+          <input
+            className={styles.fromInput}
+            id="password"
+            type="password"
+            autoComplete="off"
+          />
+        </div>
+        <button className={styles.formButton}>LOGIN</button>
+      </form>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,14 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import React from "react";
-import QuestionBox from "@/components/QuestionBox";
+import LoginForm from "@/components/LoginForm";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className={inter.className + " pagina"}>
-      <QuestionBox
-        theme={"ligth"}
-        question={"Que es TypeScript"}
-        answer={
-          "TypeScript es un lenguaje de programacion desarrollado por Microsoft.TypeScript es un lenguaje de programacion desarrollado por MicrosoftTypeScript es un lenguaje de programacion desarrollado por MicrosoftTypeScript es un lenguaje de programacion desarrollado por MicrosoftTypeScript es un lenguaje de programacion desarrollado por Microsoft"
-        }
-        num={1}
-      />
-      
+      <LoginForm theme={"ligth"} />
     </div>
   );
 }
