ccba59aff95211db20b3fa1e7e74cd41cb518db3
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-08-03 14:31:25 -0300

    Cambio de favicon. Correccion de bugs en Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,31 @@
-import { TypeTheme } from '@/libs/interfaces'
-import React from 'react'
-import styles from '@/styles/Componentes.module.css'
+import { TypeTheme } from "@/libs/interfaces";
+import React from "react";
+import styles from "@/styles/Componentes.module.css";
 
-const Footer: React.FC<TypeTheme> = ( props ) => {
+const Footer: React.FC<TypeTheme> = (props) => {
   return (
     <div className={styles.footer}>
       <p className={styles.footerTitle}>Valentin Libonati</p>
       <div className={styles.footerSocial}>
-        <a className={styles.footerLink} href="https://www.linkedin.com/in/valentin-libonati-b608521b7/">Mi Linkedin</a>
-        <a className={styles.footerLink} href="https://valentinlibonat33.vercel.app/">Mi Portfolio</a>
+        <a
+          className={styles.footerLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/valentin-libonati-b608521b7/"
+        >
+          Mi Linkedin
+        </a>
+        <a
+          className={styles.footerLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://valentinlibonat33.vercel.app/"
+        >
+          Mi Portfolio
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
